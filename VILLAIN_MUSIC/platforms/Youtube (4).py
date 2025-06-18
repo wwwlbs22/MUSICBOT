1b0a43576163de73b2bb3eb485767c08dce04072
VILLAIN MUSIC <ramnik5911@gmail.com>
2025-06-18 11:41:57 +0530

    Update Youtube.py

--- VILLAIN_MUSIC/platforms/Youtube (4).py
+++ VILLAIN_MUSIC/platforms/Youtube (4).py
@@ -8,8 +8,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from SONALI.utils.database import is_on_off
-from SONALI.utils.formatters import time_to_seconds
+from VILLAIN_MUSIC.utils.database import is_on_off
+from VILLAIN_MUSIC.utils.formatters import time_to_seconds
 
 async def shell_cmd(cmd):
     proc = await asyncio.create_subprocess_shell(
@@ -26,7 +26,7 @@ async def shell_cmd(cmd):
     return out.decode("utf-8")
 
 
-cookies_file = "SONALI/assets/cookies.txt"
+cookies_file = "VILLAIN_MUSIC/assets/cookies.txt"
 
 class YouTubeAPI:
     def __init__(self):

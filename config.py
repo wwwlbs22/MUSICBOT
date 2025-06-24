a862b5472f47195b877fb77b5f54f6cbad41fda8
ZORO7861 <prootaku786@gmail.com>
2025-06-24 18:16:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "",
+    "https://github.com/ZORO7861/aJ/tree/main",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
